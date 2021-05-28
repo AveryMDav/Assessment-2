3240402f0ae888c02c4387b7e8723d15d9f7a219
Avery.Davenport <averymdavenport@gmail.com>
2021-05-28 11:42:29 -0600

    third-commit

--- password.js
+++ password.js
@@ -1,1 +1,19 @@
 console.log('Welcome to the Password Validator Tool')
+
+const readline = require('readline');
+
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+reader.question('Please enter your password.', function(input){
+    let pass = input.slice(' ');
+
+    if (pass.length >= 10){
+        console.log('Success!')
+    }else{
+        console.log('Failed.')
+    }
+    reader.close()
+})

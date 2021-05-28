7c8b772971821c86bae8657228f9a254bd060264
Avery.Davenport <averymdavenport@gmail.com>
2021-05-28 12:06:38 -0600

    fourth-commit

--- password.js
+++ password.js
@@ -7,13 +7,34 @@ const reader = readline.createInterface({
     output: process.stdout
 })
 
+// reader.question('Please enter your password.', function(input){
+//     let pass = input.slice(' ');
+
+//     if (pass.length >= 10){
+//         console.log('Success!')
+//     }else{
+//         console.log('Failed.')
+//     }
+//     reader.close()
+// })
+
+
 reader.question('Please enter your password.', function(input){
     let pass = input.slice(' ');
+    let vowel = ['a','e','i','o','u','A','E','I','O','U']
+    let count = 0
 
     if (pass.length >= 10){
-        console.log('Success!')
-    }else{
-        console.log('Failed.')
+        for (i=0; i < pass.length; i++){
+            if(vowel.includes(pass[i])){
+                count++; 
+            } 
+        } if (count >= 1) {
+            console.log ('Success')
+        } else {
+            console.log ('Failure')
+        }
+    } else { console.log ('Failure')
     }
     reader.close()
 })
